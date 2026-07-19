265ab883581eeafc964fb6f1a697562ddebf7bdc
bhargavasrivarma-oss <bhargavasrivarma@gmail.com>
2026-07-19 14:12:36 -0400

    Update base path in vite.config.js for production

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    base= '/Bhargava-portfolio/'
+    base: process.env.NODE_ENV === 'production'
+    ? '/Bhargava-portfolio/'
+    : '/',
   ],
 })
